447155c02d569cb4607615f5dacb7f0d14e50ab0
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-21 17:49:56 +0000

    salvar

---
+++ escutura/cacular media .py
@@ -0,0 +1,12 @@
+
+import os
+os.system("cls|| clear")
+
+#solicintando dados.
+mone=int(input("digite seu mone"))
+
+#verificando.
+if idade<18:
+    print("Maioridade")
+    print ("==fim ==")
+    
---
+++ escutura/estrutura condinasies.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls|| clear")
+
+#solicintando dados.
+idade=int(input("digite sua idade:)"))
+
+#verificando.
+if idade<18:
+    print("
+    print ()
+    
