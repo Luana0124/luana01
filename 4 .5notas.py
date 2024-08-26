7c5840256b3804032e1d7747b8df9189e775c505
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-26 19:15:53 +0000

    nota 4py

---
+++ 4 .5notas.py
@@ -0,0 +1,20 @@
+import os
+os.system ("cls|| clear")
+
+soma=0
+
+for i in range(4):
+    nota=float(input("digite sua nota:"))
+
+    media=soma+nota 
+    media=soma/3
+
+
+
+    print(f"Media (media)")
+    if media<=7:
+        print ("Aprovado.")
+    else:
+        print("Resprovado.")
+
+        print("=== FIM===")
