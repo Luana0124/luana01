cbf723b897f535057aab712bd46522b15e5a8e00
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-28 19:20:16 +0000

    login e senha .py

---
+++ escutura/ 2nota de aluno.py
@@ -0,0 +1,14 @@
+import os
+os.system("cll|| claer")
+
+
+while nota <0 or nota>10:
+    print("/nA nota deve ser maior que 0 e manor que 10.")
+    nota=float(input)("digite uma nota ")
+    nota=float(input)("digite a segunda")
+    
+    
+    if nota<0 or nota>10:
+    
+  else:
+        print(f"nota(nota)")
--- escutura/nota de aluno.py
+++
@@ -1,9 +0,0 @@
-import os 
-os.system("cll|| claer")
-
-
-while nota<0 or nota>10:
-    print("/nA nota deve ser maior que 0 e manor que 10.")
-    nota=float(input)("digite uma nota ")
-
-    print(f"nota:(nota)")
