898bec354efd16029a84ba68a5bf129b4ecb9863
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-28 17:06:33 +0000

    nota de aluno.py

---
+++ escutura/nota de aluno.py
@@ -0,0 +1,9 @@
+import os 
+os.system("cll|| claer")
+
+
+while nota<0 or nota>10:
+    print("/nA nota deve ser maior que 0 e manor que 10.")
+    nota=float(input)("digite uma nota ")
+
+    print(f"nota:(nota)")
