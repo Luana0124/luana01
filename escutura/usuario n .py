040c34598b6d4f5bcb7de1c0cf57a512e650ebfa
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-30 19:31:57 +0000

    uruario.py

--- escutura/usuario n .py
+++ escutura/usuario n .py
@@ -5,11 +5,13 @@ contador=0
 soma=0
 while True:
     inserir_nota=input("digite uma nota desejada:")
-    if inserir_nota=="N":
-        media=soma/contador
-        print ("fmedia (media:)")
-        break
-    else:
-        contador=contador+1
-        soma=contador/soma
+    contador+=1
+    soma+inserir_nota
+    resposta=input(" deseja inserir mais nota?:")
+    if resposta=="N":
+     break
+    media=soma/contador
+    print(f"Media(media)")
+
+
 
