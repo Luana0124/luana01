3dcc22e17f7f76c2bf5a6182de16c9650f52777b
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-30 19:50:28 +0000

    luana.py 30/08

---
+++ escutura/luana.py
@@ -0,0 +1,9 @@
+import os 
+os.system("cls||claer")
+contador=0
+
+while True:
+    print("deseja inseir mais nota?")
+    ("digite a opcao deseja:" )
+    S=inserir mais uma nota
+    P=ver quantas 
