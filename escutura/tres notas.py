67bebd1b0b20713e1e3c9410c2a9f1814436db86
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-08-30 17:10:14 +0000

    tres notas.py

--- escutura/tres notas.py
+++ escutura/tres notas.py
@@ -2,13 +2,22 @@ import os
 os.system("cll|| clear")
 soma=0
 media=0
-nota=float(input("digite primeria nota:")
-nota=float(input("digite a segunda nota:")
-nota=float(input("digite a terceira nota:")
+a=3
 
-for i in renge(3):
+for i in range(a):
+    nota=float(input("digite sua nota"))
+    while True:
+        if nota<0 or nota>10:
+            print("nota incoreta:")
+            break
+        else:
+            break
+    soma=nota+soma
+    media=soma/a
+print(soma,media)
 
-
-
-    
+if media>7:
+   print(" aprovado")
+   if media<0:
+       print("reprovado")
 
