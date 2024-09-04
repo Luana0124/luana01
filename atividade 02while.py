2c38a28e7c009e4f89efb667cc29348475d0ca74
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-09-04 16:27:32 +0000

    codigo .py

---
+++ atividade 02while.py
@@ -0,0 +1,7 @@
+import os
+os.system("cll clear")
+
+numero=51
+while numero % 7 != 0:
+     numero+=1
+print(f"o primerio maior que 50 e divisivel por 7 è:{numero}")
---
+++ senha.py
@@ -0,0 +1,11 @@
+import os 
+os. system("cll|| claer")
+
+
+while True:
+     input("digite sua senha:")
+     print( "input(senha_salva")
+
+else:
+break:
+ print( senha_salva)
