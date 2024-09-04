1faea00ffb8de0e7e129c5a81c8d75ceaadde5fe
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-09-04 16:42:26 +0000

    calorias

---
+++ calorias.py
@@ -0,0 +1,14 @@
+import os 
+os.system("cll||claer")
+
+calorias=0
+calorias_total=200
+
+while True:
+    calorias_total<=200:
+    calorias=input(" digite sua caloria por reifeicao:")
+    if calorias<0:
+        print(" o numero de cada refeicao:")
+        calorias_total+=calorias
+    else:
+        break:
