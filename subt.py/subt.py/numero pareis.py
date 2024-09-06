1e3f81da3e7a42d1786203608fecaa8b3e6a5d5f
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-09-06 19:32:11 +0000

    numero par impar

---
+++ subt.py/subt.py/numero pareis.py
@@ -0,0 +1,17 @@
+import os 
+os.system(" clls||clear")
+
+#processamento.
+def verficar_numero(numero):
+    if numero%2==0:
+        print("par.")    
+    else:
+
+        print("impar.")
+
+#Entrada de dados.
+print("===solicitando dados===")
+numero=int(input("digite um numero:"))
+
+#saida.
+verficar_numero(numero)
