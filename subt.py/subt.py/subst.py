8b4a69fcccacd153e02e60897b588f1fc5e1cd5e
Luana0124 <luanadossantosconceicao24.thay@gmail.com>
2024-09-06 18:06:16 +0000

    primerio_nunero

---
+++ subt.py/subt.py/subst.py
@@ -0,0 +1,22 @@
+import os
+os.system(" cl|| clear")
+
+#solicidado dado. 
+primerio_numero=int(input("digite o primerio:"))
+segundo_numero=(int("digite o segudo numero:"))
+
+#processamento:
+def somar=somar(primerio_numero,segundo_numero)
+def subtracao=subtrair(primerio_numero, segundo_numero)
+def multiplicao = mutiplicar(primerio_numero, segundo_numero)def divisao=dividir(primerio_numero, segundo_numero)
+
+# exibindo dados.
+
+print("/n=== exibindo resutados ===")
+print(f"soma:{soma}")
+print(f"soma:{subtracao}")
+print(f" soma{multiplicao}")
+print(f"soma:{divisao}")
+
+
+
